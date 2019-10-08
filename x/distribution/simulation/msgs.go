00459d1cf6e434dfd436e764ce1c9e0db1570e9e
Joe Bowman <joe@chorus.one>
2019-10-08 11:19:10 +0200

    Don't simulate withdrawing rewards (because they don't exist!)

--- x/distribution/simulation/msgs.go
+++ x/distribution/simulation/msgs.go
@@ -38,31 +38,6 @@ func SimulateMsgSetWithdrawAddress(m auth.AccountKeeper, k distribution.Keeper) 
 	}
 }
 
-// SimulateMsgWithdrawDelegatorReward generates a MsgWithdrawDelegatorReward with random values.
-func SimulateMsgWithdrawDelegatorReward(m auth.AccountKeeper, k distribution.Keeper) simulation.Operation {
-	handler := distribution.NewHandler(k)
-	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context,
-		accs []simulation.Account) (opMsg simulation.OperationMsg, fOps []simulation.FutureOperation, err error) {
-
-		delegatorAccount := simulation.RandomAcc(r, accs)
-		validatorAccount := simulation.RandomAcc(r, accs)
-		msg := distribution.NewMsgWithdrawDelegatorReward(delegatorAccount.Address, sdk.ValAddress(validatorAccount.Address))
-
-		if msg.ValidateBasic() != nil {
-			return simulation.NoOpMsg(distribution.ModuleName), nil, fmt.Errorf("expected msg to pass ValidateBasic: %s", msg.GetSignBytes())
-		}
-
-		ctx, write := ctx.CacheContext()
-		ok := handler(ctx, msg).IsOK()
-		if ok {
-			write()
-		}
-
-		opMsg = simulation.NewOperationMsg(msg, ok, "")
-		return opMsg, nil, nil
-	}
-}
-
 // SimulateMsgWithdrawValidatorCommission generates a MsgWithdrawValidatorCommission with random values.
 func SimulateMsgWithdrawValidatorCommission(m auth.AccountKeeper, k distribution.Keeper) simulation.Operation {
 	handler := distribution.NewHandler(k)
