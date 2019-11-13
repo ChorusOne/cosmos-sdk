32c62b1860e5e57c2f27c3cb09d03cf3870068fc
Joe Bowman <joe@chorus.one>
2019-11-13 17:25:33 +0000

    use a waitgroup, and kick blockers off as go-routines

--- cmd/gaia/app/extension.go
+++ cmd/gaia/app/extension.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"database/sql"
+	"sync"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tendermint/tendermint/abci/types"
@@ -11,9 +12,13 @@ func (app *GaiaApp) BeginBlockHook(database *sql.DB, blockerFunctions []func(*Ga
 	return func(ctx sdk.Context, req types.RequestBeginBlock) types.ResponseBeginBlock {
 		res := app.BeginBlocker(ctx, req)
 		// fucntions
+		wg := sync.WaitGroup{}
 		for _, fn := range blockerFunctions {
-			fn(app, database, ctx, vals, accs, network, chainid, req)
+			wg.Add(1)
+			go fn(app, database, ctx, vals, accs, network, chainid, req, wg)
+
 		}
+		wg.Wait()
 		return res
 	}
 }
