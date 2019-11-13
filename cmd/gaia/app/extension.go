61022fd5120b55aa1fa4c7491dc8c8c052b156fb
Joe Bowman <joe@chorus.one>
2019-11-13 17:35:35 +0000

    fix waitgroup signature

--- cmd/gaia/app/extension.go
+++ cmd/gaia/app/extension.go
@@ -8,14 +8,14 @@ import (
 	"github.com/tendermint/tendermint/abci/types"
 )
 
-func (app *GaiaApp) BeginBlockHook(database *sql.DB, blockerFunctions []func(*GaiaApp, *sql.DB, sdk.Context, []sdk.ValAddress, []sdk.AccAddress, string, string, types.RequestBeginBlock), vals []sdk.ValAddress, accs []sdk.AccAddress, network string, chainid string) sdk.BeginBlocker {
+func (app *GaiaApp) BeginBlockHook(database *sql.DB, blockerFunctions []func(*GaiaApp, *sql.DB, sdk.Context, []sdk.ValAddress, []sdk.AccAddress, string, string, types.RequestBeginBlock, *sync.WaitGroup), vals []sdk.ValAddress, accs []sdk.AccAddress, network string, chainid string) sdk.BeginBlocker {
 	return func(ctx sdk.Context, req types.RequestBeginBlock) types.ResponseBeginBlock {
 		res := app.BeginBlocker(ctx, req)
 		// fucntions
 		wg := sync.WaitGroup{}
 		for _, fn := range blockerFunctions {
 			wg.Add(1)
-			go fn(app, database, ctx, vals, accs, network, chainid, req, wg)
+			go fn(app, database, ctx, vals, accs, network, chainid, req, &wg)
 
 		}
 		wg.Wait()
